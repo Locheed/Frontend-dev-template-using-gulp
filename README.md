78073bcc03509cd11a50382a16b0c7c2e3622273
Mika Vesasto <locheed@gmail.com>
2016-08-12 12:44:06 +0300

    Added plumber and coffee plugins

--- README.md
+++ README.md
@@ -8,13 +8,15 @@ This gulpfile.js and package.JSON is my personal configuration files for my Gulp
   -  [Autoprefixer](https://github.com/postcss/autoprefixer) - plugin to parse CSS and add vendor prefixes to CSS rules using values from Can I Use.
   -  [BrowserSync](https://github.com/BrowserSync/browser-sync) - Keep multiple browsers & devices in sync when building websites.
   -  [CSSnano](https://github.com/ben-eb/cssnano) - A modular minifier, built on top of the PostCSS ecosystem.
+  -  [Gulp-coffee](https://github.com/contra/gulp-coffee) - gulp-coffee will emit an error for cases such as invalid coffeescript syntax. If uncaught, the error will crash gulp.
   -  [Gulp-del](https://github.com/gulpjs/gulp/blob/master/docs/recipes/delete-files-folder.md) - Deleting unnecessary files before building.
   -  [Gulp-if](https://github.com/robrich/gulp-if) - A ternary gulp plugin: conditionally control the flow of vinyl objects.
-  -  [postCSS](https://github.com/postcss/postcss) - PostCSS is a tool for transforming styles with JS plugins.
-  -  [Run-sequence](https://github.com/OverZealous/run-sequence) - Runs a sequence of gulp tasks in the specified order.
+  -  [Gulp-plumber](https://github.com/floatdrop/gulp-plumber) - Prevent pipe breaking caused by errors from gulp plugins
   -  [Gulp-SASS](https://github.com/dlmanning/gulp-sass) - Sass plugin for Gulp.
   -  [Gulp-sourcemaps](https://github.com/floridoo/gulp-sourcemaps) - Write inline source maps
   -  [Gulp-uglify](https://github.com/terinjokes/gulp-uglify) - Minify JavaScript with UglifyJS2.
+  -  [postCSS](https://github.com/postcss/postcss) - PostCSS is a tool for transforming styles with JS plugins.
+  -  [Run-sequence](https://github.com/OverZealous/run-sequence) - Runs a sequence of gulp tasks in the specified order.
   -  [Useref](https://github.com/jonkemp/useref) - Parse build blocks in HTML files to replace references
 
   ### Installation
--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var gulp          = require('gulp'),
     autoprefixer  = require('gulp-autoprefixer'),
     browserSync   = require('browser-sync').create(),
     runSequence   = require('run-sequence'),
+    plumber       = require('gulp-plumber'),
+    coffee        = require('gulp-coffee'),
     cssnano       = require('gulp-cssnano')
     useref        = require('gulp-useref'),
     uglify        = require('gulp-uglify'),
@@ -31,7 +33,14 @@ gulp.task('build', function (callback) {
 // ** Development plugins **
 gulp.task('sass', function() {
   return gulp.src('src/**/*.+(scss|sass')
+    .pipe(plumber({
+        handleError: function (err) {
+            console.log(err);
+            this.emit('end');
+        }
+    }))
     .pipe(sass()) // Using gulp-sass
+    .pipe(plumber.stop())
     .pipe(gulp.dest('src/css'))
     .pipe(browserSync.reload({
       stream: true
@@ -49,17 +58,31 @@ gulp.task('browserSync', function() {
 // ** Building plugins **
 gulp.task('autoprefixer', function() {
   return gulp.src('./src/*.css')
+    .pipe(plumber({
+        handleError: function (err) {
+            console.log(err);
+            this.emit('end');
+        }
+    }))
     .pipe(sourcemaps.init())
     .pipe(postcss([autoprefixer({ browsers: ['last 2 versions'] }) ]))
     .pipe(sourcemaps.write('.'))
+    .pipe(plumber.stop())
     .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('useref', function() {
   return gulp.src('src/*.html')
+  .pipe(plumber({
+        handleError: function (err) {
+            console.log(err);
+            this.emit('end');
+        }
+    }))
   .pipe(useref())
   .pipe(gulpIf('*.js', uglify())) // Minifies only if it's a JavaScript file
   .pipe(gulpIf('*.css', cssnano())) // Minifies only if it's a CSS file
+  .pipe(plumber.stop())
   .pipe(gulp.dest('dist'))
 });
 
@@ -71,3 +94,9 @@ gulp.task('fonts', function() {       // Task copies possible fonts from src to 
 gulp.task('clean:dist', function() {
   return del.sync('dist');
 });
+
+// ** Error handling plugins **
+gulp.src('./src/*.ext')
+    .pipe(plumber())
+    .pipe(coffee())
+    .pipe(gulp.dest('./dist'));
